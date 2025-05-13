00380a491a657b8176b9713bdb2e092c35ae6f84
Gizalda Risqia <gzldarsqia@gmail.com>
2025-05-12 23:40:56 -0700

    Update ai_solution.c

--- Linked List/ai_solution.c
+++ Linked List/ai_solution.c
@@ -1,1 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->next = NULL;
+    return node;
+}
+
+void printMiddle(struct Node* head) {
+    struct Node *slow = head, *fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    if (slow != NULL)
+        printf("Middle node: %d\n", slow->data);
+}
+
+void append(struct Node** head_ref, int new_data) {
+    struct Node* new_node = newNode(new_data);
+    if (*head_ref == NULL) {
+        *head_ref = new_node;
+        return;
+    }
+    struct Node* temp = *head_ref;
+    while (temp->next != NULL)
+        temp = temp->next;
+    temp->next = new_node;
+}
+
+int main() {
+    struct Node* head = NULL;
+
+    append(&head, 10);
+    append(&head, 20);
+    append(&head, 30);
+    append(&head, 40);
+    append(&head, 50);
+    append(&head, 60);
+
+    printMiddle(head);  
+
+    return 0;
+}
